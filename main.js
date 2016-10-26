6aa1e094dd9e640cea11394828c2c6bd384d7cd9
All88keys <chrisvass1@gmail.com>
2016-10-26 10:07:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 
-
 var num = 1;
 
 //function findwords(){
   var str = num.toString();
   var array = str.split("");
   console.log(str);
+  var english = [];
+  for(var i = 0; i<array.length; i++){
+    switch(array[i]){
+      case 1:
+        english[i] = "one";
+        break;
+      case 2:
+        english[i] = "two";
+        break;
+      case 3:
+        english[i] = "three";
+        break;
+       case 4:
+        english[i] = "four";
+        break;
+       case 5:
+        english[i] = "five";
+        break;
+        case 6:
+        english[i] = "six";
+        break;
+       case 7:
+        english[i] = "seven";
+        break;
+       case 8:
+        english[i] = "eight";
+        break;
+       case 9:
+        english[i] = "nine";
+        break;
+    defualt:
+        break;
+  }
+    console.log(english)
+  }
+  
 //}
